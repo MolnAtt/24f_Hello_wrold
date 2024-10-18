bb3730ca1263d9723900f97abd2ab0f94fe59f0f
Molnár Attila <molnar.attila@szlgbp.hu>
2024-10-18 10:51:47 +0200

    Add project files.

---
+++ 24f_Hello_wrold/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24f_Hello_wrold
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Hello Wrold!"); // kiír egy szöveget
+
+			//Console.ReadLine(); // ez itt bekér egy szöveget(sort) -- enterrel lesz vége!
+			Console.ReadKey(); // egyetlen billentyűt vár
+        }
+	}
+}
